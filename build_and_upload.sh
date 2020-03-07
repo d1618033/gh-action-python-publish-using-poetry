0e9d505c7e781936d6c766179365d6df1a94e02f
d1618033 <d1618033@gmail.com>
2020-03-07 13:16:02 +0200

    get version from tag

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+set -e
+
+version=$(git describe --tags)
+poetry version "${version}"
 poetry build
 TWINE_USERNAME="$INPUT_PYPI_USERNAME" TWINE_PASSWORD="$INPUT_PYPI_PASSWORD" twine upload dist/*
